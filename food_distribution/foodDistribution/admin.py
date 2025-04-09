1db2b6a4b4834fe9fd0a8648df35d4d72cd3bde3
Tankeu Ndosse Franck <tankeundossefranck@gmail.com>
2025-04-09 01:24:49 +0100

    updates on the admin.py entity registration on the admin panel

--- food_distribution/foodDistribution/admin.py
+++ food_distribution/foodDistribution/admin.py
@@ -4,3 +4,19 @@ from .models import *
 
 admin.site.register(Profile)
 admin.site.register(VolunteerProfile)
+admin.site.register(FoodStock)
+admin.site.register(Food)
+admin.site.register(MonetaryDonation)
+admin.site.register(FoodAidRequest)
+admin.site.register(DistributionPlan)
+admin.site.register(CollectionPoint)
+admin.site.register(VolunteerAssignment)
+admin.site.register(Notification)
+admin.site.register(Notification)
+admin.site.register(BeneficiaryProfile)
+admin.site.register(SupportTicket)
+
+
+
+
+
